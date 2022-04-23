8df7b8d45da7fb6982b01d105a228de0f648d779
Cristian <cristian.beltran.paco@gmail.com>
2022-04-23 14:40:32 -0400

    Inicio de programa de historias locas

---
+++ historias-locas.py
@@ -0,0 +1,11 @@
+"""Programa para aprender a concatenar cadenas de caracteres"""
+"""Este programa pedira ingresar palabras para luego concatenarlas 
+con una frase ya grabada y mostrarnos nuestra historia loca"""
+
+
+from pyparsing import original_text_for
+
+
+organizacion = "FreeCodeCamp"
+
+print("Aprende a pogramar con " + organizacion)
